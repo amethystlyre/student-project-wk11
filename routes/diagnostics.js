2f9364f65fa20f5b9981e8aec3cdd05fb0b5d594
amethystlyre <91407728+amethystlyre@users.noreply.github.com>
2023-11-04 22:23:09 +1100

    update diagnostics

--- routes/diagnostics.js
+++ routes/diagnostics.js
@@ -5,11 +5,26 @@ const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
 // GET Route for retrieving diagnostic information
 diagnostics.get('/', (req, res) => {
   // TODO: Logic for sending all the content of db/diagnostics.json
+  readFromFile('./db/diagnostics.json').then((data) => res.json(JSON.parse(data)))
 });
 
 // POST Route for a error logging
 diagnostics.post('/', (req, res) => {
   // TODO: Logic for appending data to the db/diagnostics.json file
+  const newDiagPost = {
+    time : Date.now(),
+    error_id : uuidv4(),
+    errors : req.body,
+  };
+  readAndAppend(newDiagPost, './db/diagnostics.json');
+
+  const response = {
+    status: 'success',
+    body: newDiagPost,
+  };
+
+  res.json(response);
+
 });
 
 module.exports = diagnostics;
