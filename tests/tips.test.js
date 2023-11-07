d01a4df7dcccbed201fabd387b762bcd8e7c87b3
amethystlyre <91407728+amethystlyre@users.noreply.github.com>
2023-11-07 12:15:04 +1100

    change

---
+++ tests/tips.test.js
@@ -0,0 +1,8 @@
+const tips = require('../routes/tips.js');
+
+describe('Tips', () => {
+  test('should retrieve a list of tips when using the GET method', () => {
+    
+    expect().toBe();
+  });
+});
